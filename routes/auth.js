374a92e92d35e1d63e099a76ce971a6e966e495a
ajay wilkins <santhiyahoo@yahoo.com>
2020-04-23 23:39:02 +0530

    beta version : blogs api

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Admins = require('../models/admin');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const secret = require('../config/auth');
